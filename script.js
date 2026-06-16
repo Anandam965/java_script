6b0c08aa23d703d447672f7dae9b8c7d5e6e411d
Anandam Venkatanagasai <anandamvenkatanagasai@gmail.com>
2026-06-16 10:13:11 +0530

    2nd commit

--- script.js
+++ script.js
@@ -150,10 +150,10 @@ console.log(data[0].technology[1]);
 const changecolour = ()=>{
     var dis=document.getElementById("display");
     var arr=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F  "];
-    var randomcolour="#";
+    var randomcolor="#";
     for(var i=0;i<6;i++){
         randomcolor+=arr[Math.floor(Math.random()*16)];
     }
     
-    dis.style.backgroundColor=randomcolour;
+    dis.style.backgroundColor=randomcolor;
 }
